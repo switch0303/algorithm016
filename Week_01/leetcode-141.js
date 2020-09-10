32960ab3231dcd06cf17f721dc2477a5f13ccde2
wucong <wucong@lianfan.com>
2020-09-10 21:29:18 +0800

    feat: 141. 环形链表

---
+++ Week_01/leetcode-141.js
@@ -0,0 +1,29 @@
+// 141. 环形链表
+// https://leetcode-cn.com/problems/linked-list-cycle/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+    // 快慢指针，如果快指针追上满指针一圈，则有环
+    let slow = head,
+        fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (fast === slow) {
+            return true;
+        }
+    }
+
+    return false;
+};
