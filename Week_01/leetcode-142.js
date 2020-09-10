9a65afebb0f6b8c2a5212cfe36974291b129b2a2
wucong <wucong@lianfan.com>
2020-09-10 22:26:17 +0800

    feat: 142. 环形链表 II

--- Week_01/leetcode-142.js
+++ Week_01/leetcode-142.js
@@ -14,9 +14,9 @@
  * @return {ListNode}
  */
 var detectCycle = function (head) {
-    // 快慢指针，如果快指针追上慢指针一圈，则有环
-    // 追上后，将快指针重置到head，快慢指针都每次走一步，则再次相遇时，即在环的入口
-    // 具体的证明可以参考https://leetcode-cn.com/problems/linked-list-cycle-ii/solution/huan-xing-lian-biao-ii-by-leetcode/
+    // 1.先用快慢指针（快指针每次走两步，慢指针每次走一步）检测是否有环
+    // 2.快慢指针第一次相遇则有环，此时将快指针重置到head，快慢指针均每次走一步，再次相遇的节点即为环的入口
+    // NOTE：具体证明可参考官方题解方法2 Floyd算法中的证明 https://leetcode-cn.com/problems/linked-list-cycle-ii/solution/huan-xing-lian-biao-ii-by-leetcode/
     let slow = head,
         fast = head;
     while (fast && fast.next) {
