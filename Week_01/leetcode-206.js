9d1893ac087b632dae29ce79a05f802f1efaa537
wucong <wucong@lianfan.com>
2020-09-10 20:41:45 +0800

    feat: 206. 反转链表

---
+++ Week_01/leetcode-206.js
@@ -0,0 +1,23 @@
+// 206. 反转链表
+// https://leetcode-cn.com/problems/reverse-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+    // 迭代法
+    let prev = null,
+        curr = head;
+    while (curr) {
+        [curr.next, prev, curr] = [prev, curr, curr.next];
+    }
+    return prev;
+};
