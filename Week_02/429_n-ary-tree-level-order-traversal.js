1dfd2de5109ec4e84462bafb808580631323fd94
wucong <coci315@163.com>
2020-09-18 15:05:36 +0800

    feat: 429. N叉树的层序遍历

---
+++ Week_02/429_n-ary-tree-level-order-traversal.js
@@ -0,0 +1,37 @@
+// 429. N叉树的层序遍历
+// https://leetcode-cn.com/problems/n-ary-tree-level-order-traversal/
+// https://leetcode-cn.com/problems/n-ary-tree-level-order-traversal/
+// tree, recursion
+
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+    const res = [];
+    traverse(res, root, 0);
+    return res;
+};
+function traverse(arr, node, level) {
+    if (node == null) {
+        return;
+    }
+    if (arr[level] == null) {
+        arr[level] = [];
+    }
+    arr[level].push(node.val);
+
+    if (Array.isArray(node.children)) {
+        for (let child of node.children) {
+            traverse(arr, child, level + 1);
+        }
+    }
+}
