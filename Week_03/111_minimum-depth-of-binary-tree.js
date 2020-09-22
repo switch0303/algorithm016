5f8975f9588c6ea081cbcc5fbe02715951d21ee0
switch0303 <coci315@163.com>
2020-09-22 10:45:15 +0800

    feat: 111. 二叉树的最小深度 BFS

--- Week_03/111_minimum-depth-of-binary-tree.js
+++ Week_03/111_minimum-depth-of-binary-tree.js
@@ -3,6 +3,7 @@
 // https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/
 // tree, depth-first-search, breadth-first-search
 
+// 方法一： 递归
 /**
  * Definition for a binary tree node.
  * function TreeNode(val) {
@@ -25,4 +26,43 @@ var minDepth = function (root) {
         return Math.max(left, right) + 1;
     }
     return Math.min(left, right) + 1;
+};
+
+// 方法二： BFS
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function (root) {
+    if (root == null) {
+        return 0;
+    }
+    let queue = [root];
+    let depth = 1;
+    while (queue.length > 0) {
+        // 遍历当前层
+        let len = queue.length;
+        for (let i = 0; i < len; i++) {
+            let current = queue.shift();
+            if (current.left == null && current.right == null) {
+                return depth;
+            }
+            if (current.left) {
+                queue.push(current.left);
+            }
+            if (current.right) {
+                queue.push(current.right);
+            }
+        }
+        // 进入下一层
+        depth += 1;
+    }
+    return depth;
 };
