8e242faf6036fc45a567895ca405cae65a6b0334
switch0303 <coci315@163.com>
2020-09-30 11:42:55 +0800

    feat: 455. 分发饼干

---
+++ Week_04/455_assign-cookies.js
@@ -0,0 +1,23 @@
+// 455. 分发饼干
+// https://leetcode.com/problems/assign-cookies/discuss/
+// https://leetcode-cn.com/problems/assign-cookies/
+// greedy
+
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function (g, s) {
+    g.sort((a, b) => a - b);
+    s.sort((a, b) => a - b);
+    let i = 0,
+        j = 0;
+    while (i < g.length && j < s.length) {
+        if (s[j] >= g[i]) {
+            i++;
+        }
+        j++;
+    }
+    return i;
+};
