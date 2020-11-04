8facbfcd931d94ae44a143130a4fee8dbc2cf64c
switch0303 <coci315@163.com>
2020-11-04 11:06:58 +0800

    feat: 1122. 数组的相对排序

--- Week_08/1122_relative-sort-array.js
+++ Week_08/1122_relative-sort-array.js
@@ -1,8 +1,9 @@
 // 1122. 数组的相对排序
-// https://leetcode-cn.com/problems/relative-sort-array/
+// https://leetcode.com/problems/relative-sort-array/discuss/
 // https://leetcode-cn.com/problems/relative-sort-array/
 // hash-table, binary-search
 
+// 方法一
 /**
  * @param {number[]} arr1
  * @param {number[]} arr2
@@ -28,4 +29,31 @@ var relativeSortArray = function (arr1, arr2) {
         }
     }
     return res;
+};
+
+// 方法二：使用自带的排序方法
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @return {number[]}
+ */
+var relativeSortArray = function (arr1, arr2) {
+    let hash = {},
+        len = arr2.length;
+    for (let i = 0; i < len; i++) {
+        hash[arr2[i]] = i;
+    }
+    return arr1.sort((a, b) => {
+        let indexA = hash[a],
+            indexB = hash[b];
+        if (indexA == null && indexB == null) {
+            return a - b;
+        } else if (indexA == null) {
+            return 1;
+        } else if (indexB == null) {
+            return -1;
+        } else {
+            return indexA - indexB;
+        }
+    });
 };
