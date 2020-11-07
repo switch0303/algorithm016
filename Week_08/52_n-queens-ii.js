8f27a0049cc7b6814b38d66ada9e2bd53ed6c655
switch0303 <coci315@163.com>
2020-11-07 20:59:24 +0800

    feat: 52. N皇后 II

---
+++ Week_08/52_n-queens-ii.js
@@ -0,0 +1,26 @@
+// 52. N皇后 II
+// https://leetcode-cn.com/problems/n-queens-ii/discuss/
+// https://leetcode-cn.com/problems/n-queens-ii/
+// backtracking
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var totalNQueens = function (n) {
+    let count = 0;
+    const dfs = (row, cols, pies, nas) => {
+        if (row >= n) {
+            count++;
+            return;
+        }
+        let bits = ~(cols | pies | nas) & ((1 << n) - 1);
+        while (bits) {
+            let p = bits & -bits;
+            bits &= bits - 1;
+            dfs(row + 1, cols | p, (pies | p) << 1, (nas | p) >> 1);
+        }
+    };
+    dfs(0, 0, 0, 0);
+    return count;
+};
