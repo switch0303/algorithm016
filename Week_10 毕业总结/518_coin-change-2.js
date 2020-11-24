29c935b1eb6dc3238437d811f050e9eb9c211328
switch0303 <coci315@163.com>
2020-11-24 21:19:45 +0800

    feat: 518. 零钱兑换 II

---
+++ Week_10 毕业总结/518_coin-change-2.js
@@ -0,0 +1,20 @@
+// 518. 零钱兑换 II
+// https://leetcode.com/problems/coin-change-2/discuss/
+// https://leetcode-cn.com/problems/coin-change-2/
+// dynamic-programming
+
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function (amount, coins) {
+    let dp = new Array(amount + 1).fill(0);
+    dp[0] = 1;
+    for (let coin of coins) {
+        for (let i = coin; i <= amount; i++) {
+            dp[i] += dp[i - coin];
+        }
+    }
+    return dp[amount];
+};
