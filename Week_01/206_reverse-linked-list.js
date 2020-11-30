9a76ac6480704d76498336a0841d3bfe7bc75e43
switch0303 <coci315@163.com>
2020-11-30 22:11:34 +0800

    feat: 206. 反转链表

--- Week_01/206_reverse-linked-list.js
+++ Week_01/206_reverse-linked-list.js
@@ -2,6 +2,7 @@
 // https://leetcode-cn.com/problems/reverse-linked-list/
 // linked-list
 
+// 方法一：迭代
 /**
  * Definition for singly-linked list.
  * function ListNode(val) {
@@ -21,4 +22,25 @@ var reverseList = function (head) {
         [curr.next, prev, curr] = [prev, curr, curr.next];
     }
     return prev;
+};
+
+// 方法二：递归
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+    if (!head || !head.next) return head;
+    let next = head.next,
+        reverseHead = reverseList(next);
+    head.next = null;
+    next.next = head;
+    return reverseHead;
 };
